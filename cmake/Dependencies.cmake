d12bd9c0f5c5d0cb7b1edf28f96a4d42714642ee
mario <mario.ivancik@bringauto.com>
2024-03-06 15:30:17 +0100

    update cxx helpers package

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -4,7 +4,7 @@ BA_PACKAGE_LIBRARY(zlib          v1.2.11)
 BA_PACKAGE_LIBRARY(ba-logger     v1.2.0)
 BA_PACKAGE_LIBRARY(boost         v1.78.0)
 BA_PACKAGE_LIBRARY(fleet-protocol-interface             v2.0.0 PLATFORM_STRING_MODE any_machine NO_DEBUG ON)
-BA_PACKAGE_LIBRARY(fleet-protocol-cxx-helpers-static    v1.1.0)
+BA_PACKAGE_LIBRARY(fleet-protocol-cxx-helpers-static    v1.1.1)
 
 IF (BRINGAUTO_TESTS)
     BA_PACKAGE_LIBRARY(gtest         v1.12.1)
