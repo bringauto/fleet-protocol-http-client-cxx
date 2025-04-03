b15445caa0f3d9c33359c45b006215ba1ed87bbb
Mikuláš Šťastný <mikulas.stastny@bringauto.com>
2025-04-03 15:13:16 +0200

    Update Package name, remove any_machine flag

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,10 +1,10 @@
 SET(CMAKE_FIND_USE_CMAKE_SYSTEM_PATH OFF CACHE BOOL "Prevent using system paths for finding libraries")
 
-BA_PACKAGE_LIBRARY(zlib          v1.2.11)
-BA_PACKAGE_LIBRARY(boost         v1.86.0)
-BA_PACKAGE_LIBRARY(fleet-protocol-interface             v2.0.0 PLATFORM_STRING_MODE any_machine NO_DEBUG ON)
-BA_PACKAGE_LIBRARY(fleet-protocol-cxx-helpers-static    v1.1.1)
-BA_PACKAGE_LIBRARY(cpprestsdk    v2.10.20)
+BA_PACKAGE_LIBRARY(zlib                     v1.2.11)
+BA_PACKAGE_LIBRARY(boost                    v1.86.0)
+BA_PACKAGE_LIBRARY(fleet-protocol-interface v2.0.0 NO_DEBUG ON)
+BA_PACKAGE_LIBRARY(fleet-protocol-cpp       v1.1.1)
+BA_PACKAGE_LIBRARY(cpprestsdk               v2.10.20)
 
 IF (BRINGAUTO_TESTS)
     BA_PACKAGE_LIBRARY(gtest         v1.12.1)
